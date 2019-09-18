7ef78a347b9145649cbac11567ce7d3a2cd16da9
Beck Lin <beck.lin@ISTWLAPAPPLE2029.local>
2019-09-18 13:50:19 +0800

    connect mysql

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const router = express.Router();
 console.log(__dirname);
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : 'Imtheking2',
+  database : 'user2'
+});
+
+connection.connect();
+connection.query('SELECT * FROM user', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The results is: ', results);
+});
+connection.on('error', function(err) {
+  console.log("[mysql error]",err);
+});
+
+router.get('/login', function (req, res) {
+  res.send('login');
+  const { username, password } = req.query;
+  const dbUser = {
+    username: 'admin',
+    password: '123456',
+  };
+  if(username === dbUser.username && password === dbUser.password) {
+    console.log('正確');
+    res.cookie("user", { username: username });
+    res.redirect('index');
+  } else {
+    console.log('帳密有錯');
+  }
+});
+
 app.use('/static', express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  if(req.cookies.user !== null){
+    req.user = req.cookies.user;
+  }
+  console.log(`成功${req.user}`);
+  res.send(`Hello World!${req.user}`);
 });
 
+app.use('/api/user', router);
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 }); 
