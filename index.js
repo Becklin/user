7536655b689ead31860f2b4b5c80e9d7c9a430d6
Beck Lin <beck.lin@ISTWLAPAPPLE2029.local>
2019-09-19 15:24:40 +0800

    add crud operation

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-const router = express.Router();
+const userRouter = express.Router();
 console.log(__dirname);
+
+app.use(bodyParser.json());
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
@@ -10,17 +14,64 @@ var connection = mysql.createConnection({
   password : 'Imtheking2',
   database : 'user2'
 });
-
 connection.connect();
-connection.query('SELECT * FROM user', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The results is: ', results);
-});
 connection.on('error', function(err) {
-  console.log("[mysql error]",err);
+  if(!err) 
+    console.log('連線connection success');
+  else
+    console.log("連線connection fail : ", err);
 });
 
-router.get('/login', function (req, res) {
+//get User list
+userRouter.get('/', function (req, res) {
+  const Read = 'SELECT * FROM user';
+  connection.query( Read, (error, list, fields) => {
+    if (error) throw error;
+    res.send(list);
+  });
+});
+//get User By username
+userRouter.get('/:username', function (req, res) {
+  const ReadUser = `SELECT * FROM user WHERE username = '${req.params.username}'`;
+  connection.query( ReadUser, (error, info, fields) => {
+    if (error) throw error;
+    const data = {
+      username: info[0].username,
+      email: info[0].email,
+    };
+    res.send(data);
+  });
+});
+//insert User
+userRouter.post('/', function (req, res) {
+  const Create = `INSERT INTO user (username, email, password, userStatus) 
+  VALUES ('${req.body.username}', '${req.body.email}', '${req.body.password}', '${req.body.userStatus}')`;
+  const CheckExist = `SELECT * FROM user WHERE username = ${req.body.username}`;
+  if(CheckExist) return res.end('please change another name');
+  connection.query(Create),
+  function (error, info, fields) {
+    if (error) throw error;
+    res.send(info);
+  };
+});
+userRouter.put('/:username', function (req, res) {
+  const Update = `UPDATE user
+          SET username = '${req.body.username}', email = '${req.body.email}', password = '${req.body.password}', userStatus = '${req.body.userStatus}'
+          WHERE username = '${req.body.username}'`;
+
+  connection.query(Update), (error, info, fields) => {
+    if (error) throw error;
+    res.send(info);
+  };
+});
+userRouter.delete('/:username', function (req, res) {
+  const Delete = `DELETE FROM user WHERE username = '${req.params.username}'`;
+  connection.query( Delete, (error, info, fields) => {
+    if (error) throw error;
+    res.send('Delete success');
+  });
+});
+userRouter.get('/login', function (req, res) {
   res.send('login');
   const { username, password } = req.query;
   const dbUser = {
@@ -46,7 +97,7 @@ app.get('/', function (req, res) {
   res.send(`Hello World!${req.user}`);
 });
 
-app.use('/api/user', router);
+app.use('/api/user', userRouter);
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 }); 
